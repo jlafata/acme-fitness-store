7055432991f808f9f132737f95afe15c2b0a0b41
John LaFata <jlafata@vmware.com>
2023-01-26 14:57:22 -0500

    add tap deployment artifacts

---
+++ tap/setup-env-variables.sh
@@ -0,0 +1,8 @@
+
+export CART_SERVICE_APP="cart-service"
+export IDENTITY_SERVICE_APP="identity-service"
+export ORDER_SERVICE_APP="order-service"
+export PAYMENT_SERVICE_APP="payment-service"
+export CATALOG_SERVICE_APP="catalog-service"
+export FRONTEND_APP="frontend"
+
